7079f5a6dadbfdeaf55c0ea3ecdd6bdf8c9aa56c
Bhanoj Addala <bhanojaddala792000@gmail.com>
2025-12-01 23:19:59 +0530

    RotatingFileHandler added

--- backend_utils/logging_setup.py
+++ backend_utils/logging_setup.py
@@ -1,26 +1,27 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
 def get_logger(name: str):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s"
+    )
+    # Rotating File Handler
+    file_handler = RotatingFileHandler("app.log",maxBytes=2000,backupCount=3)
+    file_handler.setFormatter(formatter)
 
-    # Prevent duplicate handlers
-    if not logger.handlers:
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter(
-            "%(asctime)s - %(levelname)s - %(message)s"
-        )
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        file_handler = logging.FileHandler("app.log")
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+    # Stream Handler (console)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
 
     return logger
 
 
 # Run this file directly to test
 if __name__ == "__main__":
-    log = get_logger("demo_logger")
-    log.info("Information message")
-    log.error("Error message")
+    logger = get_logger("")
+    for i in range(200):
+        logger.info(f"Log entry {i}")
